fbc00bce9ef73b156f947b8937e5cd4eaec7b05d
Pritam kumar <pritam683.be22@chitkara.edu.in>
2025-07-29 20:41:57 +0530

    Fix: Use Railway-assigned port

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+
+
 const PORT = process.env.PORT || 3000;
+
+
 
 app.post('/bfhl', (req, res) => {
   const input = req.body.data;
@@ -49,4 +53,5 @@ app.post('/bfhl', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 module.exports = app; // Export the app for testing purposes
